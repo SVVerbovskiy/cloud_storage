036692346dae3b4f4618d9a1550bc7c6009deca4
SVVerbovskiy <verbovskii@inbox.ru>
2024-02-24 22:46:51 +0500

    init 2

--- book_cloud/cloud/random_string.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-
-def get_random_string(l):
-    letters = string.ascii_lowercase
-    random_string = ''.join(random.choice(letters) for i in range(l))
-    
-    return random_string
