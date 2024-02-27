df08e0e4d2990af9f263a0adefcff4947b84c62e
SVVerbovskiy <verbovskii@inbox.ru>
2024-02-27 23:38:41 +0500

    some changes

--- booking_cloud/settings.py
+++ booking_cloud/settings.py
@@ -25,11 +25,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
+SECURE_CROSS_ORIGIN_OPENER_POLICY=None
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
